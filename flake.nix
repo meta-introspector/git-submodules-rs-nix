66c15881dcd0a8c4416d7bf67782541418258a2b
mike <h@solfunmeme.com>
2025-09-10 13:35:22 +0000

    feat: Add submodules and nix flake for testing

---
+++ flake.nix
@@ -0,0 +1,60 @@
+
+{
+  description = "A flake for testing git submodules";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    gitoxide = {
+      url = "path:./gitoxide";
+      flake = false;
+    };
+    submod = {
+      url = "path:./submod";
+      flake = false;
+    };
+    magoo = {
+      url = "path:./magoo";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, gitoxide, submod, magoo }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      checks = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        {
+          gitoxide = pkgs.runCommand "gitoxide-tests" {
+            src = gitoxide;
+            buildInputs = [ rust-toolchain ];
+          } ''
+            cd $src
+            cargo test
+            touch $out
+          '';
+          submod = pkgs.runCommand "submod-tests" {
+            src = submod;
+            buildInputs = [ rust-toolchain ];
+          } ''
+            cd $src
+            cargo test
+            touch $out
+          '';
+          magoo = pkgs.runCommand "magoo-tests" {
+            src = magoo;
+            buildInputs = [ rust-toolchain ];
+          } ''
+            cd $src
+            cargo test
+            touch $out
+          '';
+        });
+    };
+}
