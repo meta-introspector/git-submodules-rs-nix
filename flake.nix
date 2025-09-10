999fd11dbbdc238c98a66283edcf6c3da72c5904
mike <h@solfunmeme.com>
2025-09-10 13:37:34 +0000

    fix: Use rust-overlay to build rust toolchain

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
 
     gitoxide = {
       url = "path:./gitoxide";
@@ -19,47 +21,44 @@
     };
   };
 
-  outputs = { self, nixpkgs, gitoxide, submod, magoo }@inputs:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, gitoxide, submod, magoo }@inputs:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      checks = forAllSystems (system:
+      forAllSystems = flake-utils.lib.eachDefaultSystem (system:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
-          rust-toolchain = pkgs.callPackage (
-            { rust-bin }:
-            rust-bin.fromRustupToolchainFile {
-              file = ./rust-toolchain.toml;
-            }
-          ) {};
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
         in
         {
-          gitoxide = pkgs.runCommand "gitoxide-tests" {
-            src = gitoxide;
-            buildInputs = [ rust-toolchain ];
-          } ''
-            cd $src
-            cargo test
-            touch $out
-          '';
-          submod = pkgs.runCommand "submod-tests" {
-            src = submod;
-            buildInputs = [ rust-toolchain ];
-          } ''
-            cd $src
-            cargo test
-            touch $out
-          '';
-          magoo = pkgs.runCommand "magoo-tests" {
-            src = magoo;
-            buildInputs = [ rust-toolchain ];
-          } ''
-            cd $src
-            cargo test
-            touch $out
-          '';
+          checks = {
+            gitoxide = pkgs.runCommand "gitoxide-tests" {
+              src = gitoxide;
+              buildInputs = [ toolchain ];
+            } ''
+              cd $src
+              cargo test
+              touch $out
+            '';
+            submod = pkgs.runCommand "submod-tests" {
+              src = submod;
+              buildInputs = [ toolchain ];
+            } ''
+              cd $src
+              cargo test
+              touch $out
+            '';
+            magoo = pkgs.runCommand "magoo-tests" {
+              src = magoo;
+              buildInputs = [ toolchain ];
+            } ''
+              cd $src
+              cargo test
+              touch $out
+            '';
+          };
         });
-    };
+    in
+    forAllSystems;
 }
