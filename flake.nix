67b04c310f62079cb3f713b4c3197d57f7eed742
mike <h@solfunmeme.com>
2025-09-10 14:14:30 +0000

    fix: Revert to using submodule inputs directly

--- flake.nix
+++ flake.nix
@@ -6,9 +6,22 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+
+    gitoxide = {
+      url = "path:./gitoxide";
+      flake = false;
+    };
+    submod = {
+      url = "path:./submod";
+      flake = false;
+    };
+    magoo = {
+      url = "path:./magoo";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }@inputs:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, gitoxide, submod, magoo }@inputs:
     let
       forAllSystems = flake-utils.lib.eachDefaultSystem (system:
         let
@@ -21,26 +34,26 @@
         {
           checks = {
             gitoxide = pkgs.runCommand "gitoxide-tests" {
-              src = self;
+              src = gitoxide;
               buildInputs = [ toolchain ];
             } ''
-              cd $src/gitoxide
+              cd $src
               cargo test
               touch $out
             '';
             submod = pkgs.runCommand "submod-tests" {
-              src = self;
+              src = submod;
               buildInputs = [ toolchain ];
             } ''
-              cd $src/submod
+              cd $src
               cargo test
               touch $out
             '';
             magoo = pkgs.runCommand "magoo-tests" {
-              src = self;
+              src = magoo;
               buildInputs = [ toolchain ];
             } ''
-              cd $src/magoo
+              cd $src
               cargo test
               touch $out
             '';
