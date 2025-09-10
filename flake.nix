3edc1e5219b14e122c6c114a9e9e4c637cf82d04
mike <h@solfunmeme.com>
2025-09-10 13:35:59 +0000

    fix: Correctly specify rust-toolchain in flake.nix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, gitoxide, submod, magoo }:
+  outputs = { self, nixpkgs, gitoxide, submod, magoo }@inputs:
     let
       systems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
@@ -28,33 +28,38 @@
       checks = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          rust-toolchain = pkgs.callPackage (
+            { rust-bin }:
+            rust-bin.fromRustupToolchainFile {
+              file = ./rust-toolchain.toml;
+            }
+          ) {};
         in
         {
           gitoxide = pkgs.runCommand "gitoxide-tests" {
             src = gitoxide;
             buildInputs = [ rust-toolchain ];
-          } ''
+          } '''
             cd $src
             cargo test
             touch $out
-          '';
+          ''';
           submod = pkgs.runCommand "submod-tests" {
             src = submod;
             buildInputs = [ rust-toolchain ];
-          } ''
+          } '''
             cd $src
             cargo test
             touch $out
-          '';
+          ''';
           magoo = pkgs.runCommand "magoo-tests" {
             src = magoo;
             buildInputs = [ rust-toolchain ];
-          } ''
+          } '''
             cd $src
             cargo test
             touch $out
-          '';
+          ''';
         });
     };
 }
